7ecf860d8ffaf42cb9c006d3223756bca14c56e9
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-07-27 14:25:44 +0200

    Added fetchSrc.

--- CreateWorkerFromRemoteURL.js
+++ CreateWorkerFromRemoteURL.js
@@ -1,6 +1,29 @@
-function createWorkerFromRemoteURL(url=''){
-	let urlObject = URL.createObjectURL(new Blob(['importScripts("'+url+'");'], {type: 'application/javascript'}));
-	let worker = new Worker(urlObject);
-	setTimeout(()=>{URL.revokeObjectURL(urlObject);},10000); // Worker does not work if urlObject is removed to early.
-	return worker;
+function createWorkerFromRemoteURL(url='', fetchSrc=false){
+	let innerContainer = {};
+	setTimeout(()=>{URL.revokeObjectURL(innerContainer.urlObject);},10000); // Worker does not work if urlObject is removed to early.
+	if(fetchSrc){
+		let container = {};
+		fetch(url)
+		.then(response => response.text())
+		.then(text => {
+			// Based on http://jsfiddle.net/uqcFM/49/
+			window.URL = window.URL || window.webkitURL;
+			var blob;
+			try{
+				blob = new Blob([text], {type: 'application/javascript'});
+			}catch(e){
+				window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+				blob = new BlobBuilder();
+				blob.append(text);
+				blob = blob.getBlob();
+			}
+			innerContainer.urlObject = URL.createObjectURL(blob);
+			container.worker = new Worker(innerContainer.urlObject);
+		});
+		return container;
+	}else{
+		innerContainer.urlObject = URL.createObjectURL(new Blob(['importScripts("'+url+'");'], {type: 'application/javascript'}));
+		let worker = new Worker(innerContainer.urlObject);
+		return worker;
+	}
 }
